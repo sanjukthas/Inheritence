da96ad3ddabd32194b95e530b48690f69297ac16
sanjukthas <sanjuktha23@gmail.com>
2016-06-20 09:59:45 -0400

    create class

---
+++ src/Bird.java
@@ -0,0 +1,16 @@
+
+public class Bird {
+
+	public void eat (){
+		System.out.println(" Bird eats");
+
+	}
+	public void fly (){
+		System.out.println(" Bird fly");
+
+	}
+	public void sleep (){
+		System.out.println(" Bird sleep");
+
+	}
+}
